610ab5a7c5e54dfcb50e2b8ab2ad7b5ce0b5ea60
Reactive-Rei <vishalofficial18@gmail.com>
2024-07-14 01:28:00 -0400

    updated chunking.js added rag.js

--- chunking.js
+++ chunking.js
@@ -2,16 +2,18 @@ import dotenv from 'dotenv';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { promises as fs } from 'fs';
 import MistralClient from "@mistralai/mistralai";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
-const client = new MistralClient(process.env.MISTRAL_API_KEY);
+const mistralClient = new MistralClient(process.env.MISTRAL_API_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY);
 
 async function splitDocument(path) {
 
     // Read the file content
     const text = await fs.readFile(path, 'utf-8');
-    console.log(text);  // Log the content or process it as needed
+    // console.log(text);  // Log the content or process it as needed
     const splitter = new RecursiveCharacterTextSplitter({
         chunkSize: 250, // Maximum chunk size by number of characters
         chunkOverlap: 40
@@ -25,7 +27,7 @@ async function splitDocument(path) {
 const handbookChunks = await splitDocument('handbook.txt');
 
 async function createEmbeddings(chunks) {
-    const embeddings = await client.embeddings({
+    const embeddings = await mistralClient.embeddings({
         model: 'mistral-embed',
         input: chunks
     });
@@ -38,4 +40,6 @@ async function createEmbeddings(chunks) {
     return data;
 }
 
-console.log(await createEmbeddings(handbookChunks));
+const data = await createEmbeddings(handbookChunks);
+await supabase.from('handbook_docs').insert(data);
+console.log("Upload complete!");
---
+++ rag.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import MistralClient from "@mistralai/mistralai";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+
+const mistralClient = new MistralClient(process.env.MISTRAL_API_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY);
+
+// 1. Getting the user input
+const input = "December 25th is on a Sunday, do I get any extra time off to account for that?";
+
+// 2. Creating an embedding of the input
+const embedding = await createEmbedding(input);
+
+// 3. Retrieving similar embeddings / text chunks (aka "context")
+const context = await retrieveMatches(embedding);
+console.log(context);
+// 4. Combining the input and the context in a prompt 
+// and using the chat API to generate a response 
+const response = await generateChatResponse(context, input);
+
+
+async function createEmbedding(input) {
+  const embeddingResponse = await mistralClient.embeddings({
+      model: 'mistral-embed',
+      input: [input]
+  });
+  return embeddingResponse.data[0].embedding;
+}
+
+async function retrieveMatches(embedding) {
+    const { data } = await supabase.rpc('match_handbook_docs', {
+        query_embedding: embedding,
+        match_threshold: 0.78,
+        match_count: 5
+    });
+    return data.map(chunk => chunk.content).join(" ");
+}
+
+
+async function generateChatResponse(context, query) {
+
+}
